33feed56206358fa12d067b3ea2d28cf2c3f33c1
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-12-25 12:02:58 +0300

    refactor: ternary operator

--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -7,14 +7,14 @@ namespace Repository;
 public abstract class RepositoryBase<T>(RepositoryContext repositoryContext) : IRepositoryBase<T> where T : class
 {
     public IQueryable<T> FindAll(bool trackChanges) =>
-        trackChanges ?
-            repositoryContext.Set<T>() :
-            repositoryContext.Set<T>().AsNoTracking();
+        trackChanges
+            ? repositoryContext.Set<T>()
+            : repositoryContext.Set<T>().AsNoTracking();
 
     public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) => 
-        trackChanges ? 
-            repositoryContext.Set<T>().Where(expression) :
-            repositoryContext.Set<T>().Where(expression).AsNoTracking();
+        trackChanges
+            ? repositoryContext.Set<T>().Where(expression)
+            : repositoryContext.Set<T>().Where(expression).AsNoTracking();
         
     public void Create(T entity) => repositoryContext.Set<T>().Add(entity);
     public void Update(T entity) => repositoryContext.Set<T>().Update(entity);
